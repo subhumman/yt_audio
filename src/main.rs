486463b5a57c1d0ded4cd657cc4d0ca07dd527f8
Den <denis02012000@hotmail.com>
2025-08-20 16:15:08 +0300

    master/pre-tests prog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+#![allow(non_snake_case)]
+
+use anyhow::Result;
+use dowAu::{darkprince_lover, get_current_dir};
+
+#[derive(Debug)]
+struct Args {
+    url: String,
+    output: String,
+    format: String,
+}
+
+impl Args{
+    fn parse() -> Result<Self>{
+        let args = std::env::args().collect::<Vec<String>>();
+        if args.len() < 2{
+            anyhow::bail!("Usage: {} <url> [output] [format]", args[0]);
+        }
+
+        let url = args[1].clone();
+        let output = args.get(2).cloned().unwrap_or_else(|| ".".to_string());
+        let format = args.get(3).cloned().unwrap_or_else(|| "mp3".to_string());
+        Ok(Self{url, output, format})
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<()>{
+    let args = Args::parse()?;
+    get_current_dir()?;
+    darkprince_lover(&args.url, &args.format, &args.output).await
 }
